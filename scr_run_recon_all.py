27845f1b194e4e7ebc2cecba5f88575d4ae932e5
Christopher J. Bailey <bailey.cj@gmail.com>
2015-03-25 09:35:06 +0100

    fixed multiproc bugs

--- scr_run_recon_all.py
+++ scr_run_recon_all.py
@@ -1,14 +1,16 @@
 # -*- coding: utf-8 -*-
+from __future__ import print_function
 """
-Created on Thu Oct  3 13:18:16 2013
-
 @author: cjb
 """
 CLOBBER = False
-FAKE    = True
+FAKE    = False
 VERBOSE = True
 
-from __future__ import print_function
+import os, sys
+import errno
+import subprocess
+import multiprocessing
 # ENH: install "official" version of stormdb on isis/hyades
 path_to_stormdb = '/volatile/cjb/stormdb'
 sys.path.append(path_to_stormdb)
@@ -16,12 +18,21 @@ sys.path.append(path_to_stormdb)
 # change to stormdb.accss (mod. __init__.py)
 from access import Query
 
-import os
-import errno
-import subprocess
-import multiprocessing
-
-
+def _parallel_task(command):
+    """
+        General purpose method to submit Unix executable-based analyses (e.g.
+        maxfilter and freesurfer) to the shell.
+        
+        Parameters:
+            command:    The command to execute (single string)
+                                        
+        Returns:        The return code (shell) of the command
+    """
+    #proc = subprocess.Popen([fs_cmd],stdout=subprocess.PIPE, shell=True)
+    proc = subprocess.Popen([command], shell=True)
+    
+    proc.communicate()
+    return proc.returncode
 
 proj_code = 'MINDLAB2013_01-MEG-AttentionEmotionVisualTracking'
 db=Query(proj_code)
@@ -35,6 +46,10 @@ fs_params = {'input_file': None, 'use_gpu': True, 'num_threads': 2,
         'fs_bin': recon_all_bin, 'subjects_dir': subjects_dir,
         'fs_tasks': '-all', 'force': False}
 
+# beware that the total number of threads can then (at worst)
+# be n_parallel_procs * fs_params['num_threads']
+# for those fs-binaries that use openmp
+n_parallel_procs = 4
 
 included_subjects = db.get_subjects()
 fs_cmd_list = []
@@ -49,7 +64,7 @@ for subj in included_subjects:
     mr_study = db.get_studies(subj, modality='MR',unique=True)
     if not mr_study is None:
         # This is a 2D list with [series_name, series_number]
-        series = db.get_series(subj, mr_study, 'MR',verbose=verbose)
+        series = db.get_series(subj, mr_study, 'MR',verbose=False)
     #### Change this to be more elegant: check whether any item in series
     #### matches sequence_name
         for ser in series:
@@ -71,36 +86,23 @@ for subj in included_subjects:
     if fs_params['num_threads']:
         fs_cmd += ' -openmp %d ' % fs_params['num_threads']
 
-    if os.path.exists(fs_params['subjects_dir'] + '/' + subj) and not CLOBBER:
+    if os.path.exists(os.path.join(fs_params['subjects_dir'],subj)) and not CLOBBER:
         print('Subject %s appears to be done, skipping (use force to overwrite)' % subj)
             
+    # switch comments to test parallel submission
     fs_cmd_list.append(fs_cmd)
+    #fs_cmd_list.append('echo "$(hostname) proc $$ sleeping 2 sec"; sleep 2') # for testing
 
 # sending multiple processes away
 if not FAKE:
-    pool = multiprocessing.Pool(processes=n_processes)
+    pool = multiprocessing.Pool(processes=n_parallel_procs)
 
     return_codes = pool.map(_parallel_task,fs_cmd_list)
     pool.close()
     pool.join()
 
 elif VERBOSE:
-    print "The following would execute, if this were not a FAKE run:"
+    print("The following would execute, if this were not a FAKE run:")
     for cmd in fs_cmd_list:
-        print "%s" % cmd
+        print("%s" % cmd)
 
-def _parallel_task(command):
-    """
-        General purpose method to submit Unix executable-based analyses (e.g.
-        maxfilter and freesurfer) to the shell.
-        
-        Parameters:
-            command:    The command to execute (single string)
-                                        
-        Returns:        The return code (shell) of the command
-    """
-    #proc = subprocess.Popen([fs_cmd],stdout=subprocess.PIPE, shell=True)
-    proc = subprocess.Popen([command], shell=True)
-    
-    proc.communicate()
-    return proc.returncode
