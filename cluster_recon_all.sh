396d6fa90c91f35f870d3c040b46574dcc510843
mads jensen <mje.mads@gmail.com>
2015-03-25 22:02:08 +0100

    Fixed typo and small updates

--- cluster_recon_all.sh
+++ cluster_recon_all.sh
@@ -2,7 +2,7 @@
 
 # Script to run Freesufer recon-all on several subjects on the cluster 
 # version: 0.1
-# Author: mje, mads [at] cnru.dk
+# Author: mje, mads [] cnru.dk
 
 # Loop to create a script per subject and submit to cluster
 for sub in # TODO: set subject numbers
@@ -10,26 +10,28 @@ do
 
 # Assign variables for script and output files
     SCRIPT=sub_recon_${sub}.sh
-
+    sub_id=sub_${sub} # TODO: Change to proper subject name for the freesurfer SUBJECTS_DIR
+ 
 # Generate a single script
 cat << EOF > ${SCRIPT}
 #!/bin/bash
 #$ -S /bin/bash
 export TERM vt100
 # Make sure MNI and CFIN are in the path
-PATH=$PATH:/usr/local/mni/bin:/usr/local/cfin/bin:. # FIXME: Check this Path is right!
+PATH=$PATH:/usr/local/mni/bin:/usr/local/cfin/bin:.
 
 # Add Freesurfer
-export FFREESURFER_HOME=/usr/local/freesurfer # FIXME: check path
+export FREESURFER_HOME=/usr/local/freesurfer
 source $FREESURFER_HOME/SetUpFreeSurfer.sh
 
-export SUBJECTS_DIR=/project/SOMETHING # TODO: set the path 
+export SUBJECTS_DIR=/projects/SOMETHING # TODO: set the path 
+export SCRIPT_dir=/projects/MINDLAB-SOMETHING/scripts
 
 # Change to current directory.
 cd `pwd` # TODO: change to scratch or log dir
 
 # Run the program with the parameters:
-recon-all -subjid -all # TODO: add params use "no-isrunning" flag?
+recon-all -s -i -all # FIXME: find way to provide dicom for input and 
 EOF
 
 # Make the new script executable
