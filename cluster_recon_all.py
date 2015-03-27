3d3307c3b5058d441dabaad46b9dddb68937419f
Christopher J. Bailey <bailey.cj@gmail.com>
2015-03-27 10:23:32 +0100

    working example of py-based qsub

--- cluster_recon_all.py
+++ cluster_recon_all.py
@@ -7,11 +7,10 @@ FAKE    = False
 VERBOSE = True
 
 import os, sys
-import errno
+import errno # should do some error checking...
 import subprocess
-import multiprocessing
 # ENH: install "official" version of stormdb on isis/hyades
-path_to_stormdb = '/volatile/cjb/stormdb'
+path_to_stormdb = '/users/cjb/src/git/cfin-tools/stormdb'
 sys.path.append(path_to_stormdb)
 
 # change to stormdb.access (mod. __init__.py)
@@ -27,10 +26,11 @@ scratch_folder = os.path.join(proj_folder, 'scratch')
 
 recon_all_bin = '/opt/local/freesurfer-releases/5.3.0/bin/recon-all'
 subjects_dir = os.path.join(scratch_folder, 'fs_subjects_dir')
+script_dir = proj_folder+'/scripts/mads_test_scripts'
 
 included_subjects = db.get_subjects()
 # just test with first one!
-included_subjects = included_subjects[0]
+included_subjects = [included_subjects[0]]
 
 for sub in included_subjects:
 
@@ -49,16 +49,16 @@ for sub in included_subjects:
     input_dicom = T1_file_names[0] # first DICOM file
 
     bash_script = ['#!/usr/bin/env bash']
+    bash_script.append('#$ -S /bin/bash') # for qsub
+    
     # have to explicitly source bashrc (non-login shell)
     bash_script.append('source ~/.bashrc')
     bash_script.append('use mne') # set paths
     bash_script.append('export SUBJECTS_DIR='+subjects_dir)
-    bash_script.append('export SCRIPT=sub_recon_'+ sub + '.sh')
-    bash_script.append('export SCRIPT_dir='+proj_folder+'/scripts/mads_test_scripts')
 
     bash_script.append('export SUBJECT=' + sub)
 
-    bash_script.append('recon-all -s ' + sub + '-i ' + input_dicom + ' -all')
+    bash_script.append('recon-all -s ' + sub + ' -i ' + input_dicom + ' -all')
     # Can do some exit value checking too...
     #bash_script.append('if [[ $? != 0 ]] ; then exit 1; fi')
 	
@@ -70,4 +70,18 @@ for sub in included_subjects:
 #ln -s watershed/${SUBJECT}_outer_skull_surface ${SUBJECT}-outer_skull.surf
 #cd ''' + ad._project_folder
 #    bash_script.append(cmd)
-    
+
+    # we could just write each line directly into the script_file
+    # instead of first generating the bash_script list
+    # but it may be beneficial at some stage to /first/ write all
+    # the scripts and only then submit all of them at once
+    script_name = script_dir + '/sub_recon_' + sub + '.sh'
+    script_file = open(script_name, 'wt')
+    for item in bash_script:
+        script_file.write("%s\n" % item)
+    script_file.close()
+
+    # remove the echo " ... " to make this run for real...
+    qsub_cmd = 'echo "qsub -j y -q long.q ' + script_name + '"'
+    process = subprocess.Popen([qsub_cmd], shell=True)
+    process.communicate()
